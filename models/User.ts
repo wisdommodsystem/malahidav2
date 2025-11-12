f4ee2510d04ec515f4cdd3b4ed182310922062ef
wisdommodsystem <am.or79h@gmail.com>
2025-11-12 01:47:51 +0100

    Update User.ts

--- models/User.ts
+++ models/User.ts
@@ -35,7 +35,7 @@ const UserSchema: Schema = new Schema(
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+  this.password = await bcrypt.hash(this.password as string, salt);
   next();
 });
 
